aea40a60695a4bbfa70068a01ea56940c8bbee15
Manik Reddy <manikreddy7@gmail.com>
2022-08-29 23:24:32 +0530

    adding the Strings and its Methods in JS

---
+++ app.js
@@ -0,0 +1,46 @@
+// let $username = "MANIK";
+// console.log($username);
+
+// let num = 9;
+// console.log(num);
+
+//  num = 5;
+// console.log(num);
+
+// console.log(num+1); 
+
+// const r=5;
+// r=4;
+// console.log(r); 
+
+// 20 String Methods:
+
+var stringOne = "Manik is a Web Developer";
+var stringTwo = "Dhruva is a Full stack Developer";
+
+console.log(stringOne.charAt(2));
+console.log(stringOne.charCodeAt(2));
+console.log(stringOne.concat(stringTwo));
+console.log(stringOne.endsWith("e"));
+console.log(String.fromCharCode(110));
+console.log(stringTwo.includes("Full"));
+console.log(stringTwo.indexOf("Full"));
+console.log(stringTwo.lastIndexOf("r"));
+console.log(stringTwo.match(/per/g));
+console.log(stringOne.repeat(2));
+console.log(stringOne.replace(/per/g,"PER"));
+console.log(stringOne.search("per"));
+console.log(stringOne.slice(1,4));
+console.log(stringOne.split(" "));
+console.log(stringOne.startsWith("Manik"));
+console.log(stringOne.substring(2,4));
+console.log(stringOne.toLocaleLowerCase());
+console.log(stringOne.toLocaleUpperCase());
+
+var stringThree = "   thankyou   ";
+console.log(stringThree.trim());
+
+
+
+
+
